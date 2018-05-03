01f52e234e142363a705e0dc7cea4beb5c46ad29
ITAgnesmeyer <ITAgnesmeyer@users.noreply.github.com>
2018-05-03 16:02:55 +0200

    Standard und Core hinzugefügt.

---
+++ EasyXMLSerializerCore/LogEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EasyXMLSerializerCore
+{
+    /// <summary>
+    /// EventArgs for LogMessages
+    /// </summary>
+    public class LogEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Message</param>
+        public LogEventArgs(string message)
+        {
+            this.Message = message;
+        }
+
+        /// <summary>
+        /// Message
+        /// </summary>
+        public string Message { get; }
+
+    }
+}
