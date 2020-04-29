df5bf4d722bbbd1d5a97931272f887653e35c7e0
DIGA <guido.agnesmeyer@itagnesmeyer.de>
2020-04-29 11:43:31 +0200

    VS1.08

--- SRC/EasyXMLSerialzerUWP/Properties/AssemblyInfo.cs
+++ SRC/EasyXMLSerialzerUWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dipl.-Ing.(FH) Guido Agnesmeyer")]
 [assembly: AssemblyProduct("EasyXMLSerialzerUWP")]
-[assembly: AssemblyCopyright("Copyright ©  2018 Dipl.-Ing.(FH) Guido Agnesmeyer")]
+[assembly: AssemblyCopyright("© Dipl.-Ing.(FH) Guido Agnesmeyer 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7.1")]
-[assembly: AssemblyFileVersion("1.0.7.1")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
 [assembly: ComVisible(false)]
